c9cc76efc9b4e15fea0c9523642bb87e03b7b30e
Mariana Meriles <marianameriles@dhcp-vl2042-21288.redrover.cornell.edu>
2022-05-05 19:54:01 -0400

    merge test againgit add .

--- ClassRankerApp/ClassRankerApp/OpeningViewController.swift
+++ ClassRankerApp/ClassRankerApp/OpeningViewController.swift
@@ -41,7 +41,7 @@ class OpeningViewController: UIViewController {
     
     var signInLabel: UILabel = {
         let label = UILabel()
-        label.text = "Sign in"
+        label.text = "CHANGE ME BACK"
         label.textColor = .white
         label.font = UIFont(name: "Proxima Nova Bold", size: 20)
         label.textAlignment = .center
